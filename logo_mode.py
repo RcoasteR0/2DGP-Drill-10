fe55ea651dcde750778f6aaafc4b2129ac022aba
RcoasteR0 <st_anthony@naver.com>
2024-11-05 11:50:15 +0900

    play_mode

---
+++ logo_mode.py
@@ -0,0 +1,28 @@
+from pico2d import load_image, delay, clear_canvas, update_canvas, get_events, get_time
+
+def init():
+    global image
+    global running
+    global logo_start_time
+    image = load_image('tuk_credit.png')
+    running = True
+    logo_start_time = get_time()
+
+def finish():
+    global image
+    del image
+
+def update():
+    global running
+    global logo_start_time
+    if get_time() - logo_start_time >= 2.0:
+        logo_start_time = get_time()
+        running = False
+
+def draw():
+    clear_canvas()
+    image.draw(400, 300)
+    update_canvas()
+
+def handle_events():
+    events = get_events()
---
+++ play_mode.py
@@ -0,0 +1,43 @@
+from pico2d import *
+
+import game_world
+from grass import Grass
+from boy import Boy
+
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+        else:
+            boy.handle_event(event)
+
+def init():
+    global running
+    global boy
+
+    running = True
+
+    grass = Grass()
+    game_world.add_object(grass, 0)
+
+    boy = Boy()
+    game_world.add_object(boy, 1)
+
+def update():
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def finish():
+    pass
+
+
